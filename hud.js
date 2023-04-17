74ceb52ecfb6b074adef4035e844a64502521130
tahjoorona <112838468+tahjoorona@users.noreply.github.com>
2023-04-17 09:07:52 -0700

    Create hud.js

---
+++ hud.js
@@ -0,0 +1,24 @@
+function toggler() {
+	document.body.classList.toggle("dark-mode");	   
+}
+
+function draw()
+{
+	const plot = document.getElementById("plot") ;
+	const ctx = plot.getContext("2d");	
+	ctx.fillRect(50, 50, 50, 50) ;
+}
+
+function draw()
+{
+	const dspl = document.getElementById("display") ;
+	ctx = dspl.getContext("2d") ;	
+  w=disp.width;
+  h=disp.height;
+  s= w/8;
+  x=w/2 + s;
+  y= h/2 +s;
+  ctx.fillRect(x, y, s, s) ; // x y width height
+}
+
+    
